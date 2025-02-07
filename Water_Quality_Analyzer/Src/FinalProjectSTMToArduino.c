dd3474d42219f84050c3beb98b5bee2db4c38786
butle <butle@host.docker.internal>
2025-02-07 14:11:25 -0500

    Update hardware connection comments

--- Water_Quality_Analyzer/Src/FinalProjectSTMToArduino.c
+++ Water_Quality_Analyzer/Src/FinalProjectSTMToArduino.c
@@ -1,45 +1,84 @@
 /*
- * HARDWARE CONNECTIONS:
- * 		DS18B20
- * 			VCC <-> 3.3V
- * 			GND <-> GND
- * 			DQ <-> PA3 (STM32) (hanging off 4.7kOhm pull up resistor connected to +Vdd)
- *
+ * TODO HARDWARE CONNECTIONS:
+ * --MCUs--
+ * 		STM32F407
+ *			GND <-> Breadboard GND rail
+ *			+5V supply <-> Breadboard 5V supply rail
+ *			+3.3V (Vdd) supply <-> Breadboard 3.3V supply rail
+ *			PA1 <-> TDS sensor analog voltage (to ADC)
+ *			PA2 <-> Turbidity sensor analog voltage (to ADC)
+ *			PA3 <-> DQ (DS18B20) (hanging off 4.7kOhm pull up resistor connected to +Vdd)
+ *			PB6 <-> SCLK (i2c to Arduino) ~~~ Use 5V to 3.3V logic level converter to interface between the 2 boards~~~
+ *			PB7 <-> SDA (i2c to Arduino) ~~~ Use 5V to 3.3V logic level converter to interface between the 2 boards~~~
+ *		Arduino
+ *			GND <-> Breadboard GND rail
+ *			+5V supply <-> BLE 5V input
+ *			A0 <-> pH sensor analog voltage (to ADC)
+ *			A1 <-> EC sensor analog voltage (to ADC)
+ *			A5 <-> SCLK ~~~ Use 5V to 3.3V logic level converter to interface between the 2 boards~~~
+ *			A4 <-> SDA ~~~ Use 5V to 3.3V logic level converter to interface between the 2 boards~~~
+ *			Digital Pin 2 <-> BLE module UART RX
+ *			Digital Pin 3 <-> BLE module UART TX
+ * --Sensors--
  * 		TDS sensor
  * 			VCC <-> 3.3V
  * 			GND <-> GND
- * 			A <-> PA1 (STM32)
- *
+ * 			Analog signal <-> PA1 (STM32)
  * 		Turbidity sensor
+ * 			VCC <-> 3.3V
+ * 			GND <-> GND
+ * 			Analog signal <-> PA2 (STM32)
+ * 		DS18B20 (temperature sensor)
+ * 			VCC <-> 3.3V
+ * 			GND <-> GND
+ * 			DQ <-> PA3 (STM32) (hanging off 4.7kOhm pull up resistor connected to +Vdd)
+ * 		pH sensor
  * 			VCC <-> 5V
  * 			GND <-> GND
- * 			A <-> PA2 (STM32)
- *
- * 		STM32
- *			PA3 <-> DQ (DS18B20) (hanging off 4.7kOhm pull up resistor connected to +Vdd)
- *			PA1 <-> A (TDS sensor)
- *			PB6 <-> SCLK (i2c to Arduino) ~~~ Use 5V to 3.3V logic level converter to interface between the 2 boards~~~
- *			PB7 <-> SDA (i2c to Arduino) ~~~ Use 5V to 3.3V logic level converter to interface between the 2 boards~~~
- *
- *		Arduino
- *			A5 <-> SCLK ~~~ Use 5V to 3.3V logic level converter to interface between the 2 boards~~~
- *			A4 <-> SDA ~~~ Use 5V to 3.3V logic level converter to interface between the 2 boards~~~
- *
- *		LOGIC LEVEL CONVERTER
- *			HV <-> +5V (STM32)
- * 			LV <-> +3.3V (STM32)
  * 			GND <-> GND
- * 			HV1 <-> A5 (Arduino)
- *			HV2 <-> A4 (Arduino)
- *			LV1 <-> PB6 (STM32)
- *			LV2 <-> PB7 (STM32)
+ * 			Analog signal <-> A0 (Arduino)
+ * 		EC sensor
+ * 			VCC <-> 5V
+ * 			GND <-> GND
+ * 			Analog signal <-> A1 (Arduino)
+ * --Connectivity--
+ * 		3.3V/5V Logic Level Shifter
+ * 			GND <-> GND rail
+ *			HV <-> +5V rail (supplied by STM32)
+ * 			LV <-> +3.3V rail (supplied by STM32)
+ * 			HV1 <-> A5 (Arduino I2C SCL)
+ *			HV2 <-> A4 (Arduino I2C SDA)
+ *			LV1 <-> PB6 (STM32 I2C SCL)
+ *			LV2 <-> PB7 (STM32 I2C SDA)
+ * 		BLE Module
+ * 			GND <-> GND
+ * 			VCC <-> 5V
+ * 			TXD (UART) <-> Arduino digital Pin 2 (UART RX)
+ * 			RXD (UART) <-> Arduino digital Pin 3 (UART TX)
  */
 
 #include "stm32f407vg.h"
 #include "ds18b20_temp_sensor.h"
 
-
-/* TODO: Comment out "#define SEMIHOSTING_ENABLE" if not using Semihosting */
+/*
+ * TODO: Comment out "#define SEMIHOSTING_ENABLE" if not using Semihosting
+ *
+ * -- NOTE: If semihosting is ENABLED, follow these steps:
+ * 1. Add argument to linker: "-specs=rdimon.specs -lc -lrdimon" to link newlib nano standard library that supports semihosting (librdimon_nano.a)
+ * 2. Src/syscalls.c must be excluded from build - system calls are implemented by librdimon_nano.a (library supports semihosting)
+ * 3. Add "extern void initialise_monitor_handles();" to top of main.c and "iinitialise_monitor_handles(); somewhere in main() - inits semihosting
+ * 4. Add printf() statements throughout code. All must end in newline character
+ * 5. Build project
+ * 6. Debug using OpenOCD configuration and "monitor arm semihosting enable" in startup
+ *
+ *  * -- NOTE: If semihosting is DISABLED, follow these steps:
+ * 1. Remove argument to linker: "-specs=rdimon.specs -lc -lrdimon" to link newlib nano standard library (libc_nano.a)
+ * 2. Src/syscalls.c must be included in build
+ * 3. Remove "extern void initialise_monitor_handles();" to top of main.c and "iinitialise_monitor_handles();
+ * 4. Remove printf() statements throughout code (unless you are using some other implementation of printf such as ITM->SWD)
+ * 5. Build project
+ * 6. Debug using STLINK GDB server configuration and remove "monitor arm semihosting enable" in startup if present
+*/
 #define SEMIHOSTING_ENABLE
 
 #define NUM_OF_ANALOG_CONVERSIONS				2				//TDS and Turbidity
